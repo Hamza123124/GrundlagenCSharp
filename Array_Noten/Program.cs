1fb49e2ce19ffe6df08859dcd4f47b32f78bdfca
Hamza Modric <hamza.m9@gso.schule.koeln>
2022-03-10 11:13:10 +0100

    Array noten fast fertig

--- Array_Noten/Program.cs
+++ Array_Noten/Program.cs
@@ -6,7 +6,29 @@ namespace Array_Noten
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 5;
+            int summe = 0;
+
+
+            int[] noten = new int[n];
+            for (int i = 0; i < n; i++)
+
+            {
+                
+                Console.WriteLine($"Geben sie die Noten {i} ein : ");
+                noten[i] = Convert.ToInt32(Console.ReadLine());
+                summe = summe + noten[i];
+                
+            }
+            Console.WriteLine("Das sind ihre Noten:");
+            for (int p = 0; p < noten.Length; p++)
+            {  
+                Console.WriteLine(noten[p]);
+            }
+
+            Console.WriteLine("ihr durchschnitt ist :");
+            Console.WriteLine(summe/n);
+
         }
     }
 }
