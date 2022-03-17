05b62201aef68f836fcfac88f4ba0cf25b07203a
Hamza123124 <hamzamodric180@gmail.com>
2022-03-17 11:29:17 +0100

    Noten array durchschnitt

--- Array_Noten/Program.cs
+++ Array_Noten/Program.cs
@@ -6,28 +6,28 @@ namespace Array_Noten
     {
         static void Main(string[] args)
         {
-            int n = 5;
-            int summe = 0;
+            int n = 5;  //Anzahl Noten
+            int summe = 0; 
 
-
-            int[] noten = new int[n];
-            for (int i = 0; i < n; i++)
+            int[] noten = new int[n];  //Array für Noten
+            for (int i = 0; i < noten.Length ;i++)   //For schleife 
 
             {
-                
-                Console.WriteLine($"Geben sie die Noten {i} ein : ");
-                noten[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Geben sie die {i+1}. Note ein "); //Eingabe aufforderung 
+                noten[i] = Convert.ToInt32(Console.ReadLine());   //Einlese innitialisieren der Noten in Array
                 summe = summe + noten[i];
-                
-            }
-            Console.WriteLine("Das sind ihre Noten:");
-            for (int p = 0; p < noten.Length; p++)
-            {  
-                Console.WriteLine(noten[p]);
             }
 
-            Console.WriteLine("ihr durchschnitt ist :");
-            Console.WriteLine(summe/n);
+            for (int i = 0; i < noten.Length;i++)
+
+            {
+                Console.WriteLine("Ihre Noten sind :");
+                Console.WriteLine($"{i+1}.Note{noten[i]}");
+
+            }
+            Console.WriteLine($"ihr durchschnitt ist {(double) summe / n} ");
+
+
 
         }
     }
