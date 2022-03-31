a142316502bfe15652d829c9a3fc8e9b68e6fd6d
Hamza Modric <hamza.m9@gso.schule.koeln>
2022-03-31 11:53:17 +0200

    Methoden Implementation Fertiger Code

--- Methoden Implementieren/Program.cs
+++ Methoden Implementieren/Program.cs
@@ -6,28 +6,33 @@ namespace Methoden_Implementieren
     {
         static void Main()
         {
-            Console.WriteLine("Geben sie ein Wort ein");
-            String eingabe_string = Console.ReadLine();
-            Console.WriteLine("geben sie eine Zahl ein");
+            Console.WriteLine("Geben Sie einen Text ein");
+            string eingabe_str = Console.ReadLine();
+            Console.WriteLine("Geben Sie eine Zahl ein");
             int eingabe_int = Convert.ToInt32(Console.ReadLine());
 
             int anzahl = 0;
-            anzahl = ausgabeTextMalX(eingabe_string, eingabe_int);
+            anzahl = ausgabeTextMalX(eingabe_str, eingabe_int);
+
+
+
             Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
 
 
-
         }
-        public static int ausgabeTextMalX(string eingabe_string, int eingabe_int)
-
+        static int ausgabeTextMalX(string s, int i)
         {
-            System.Console.WriteLine(eingabe_string);
-            System.Console.WriteLine(eingabe_int);
 
-            return 0;
+            for (int a = 0; a < i; a++)
+            {
+
+                Console.WriteLine(s);
+            }
+            return i * s.Length;
         }
-
-
-
     }
 }
+
+        
+
+ 
